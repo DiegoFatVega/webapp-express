e7f3a78029f15c06923b44fc417f460a83ffb6bf
DiegoFatVega <fatvegadiego@gmail.com>
2025-11-17 17:24:41 +0100

    implement middleware for internal server error

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 
-
 const moviesRouter = require('./routes/movies')
+const globalMiddlewareRouter = require('./middlewares/globalMiddlewares')
 
 app.listen(PORT, () => console.log(`server is listen on port http://localhost:${PORT}`))
 
 app.get('/', (req, res) => {
+    app.diego();
     res.send("Welcome to my Movies page")
 })
 
@@ -15,7 +16,7 @@ app.use(express.static(`public`));
 
 app.use("/api/movies", moviesRouter);
 
-
+app.use(globalMiddlewareRouter.serverError);
 
 
 
